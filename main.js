f8418494b70613259b451f88b0eb63109e2ca3eb
Arteposok <aeposokhov@gmail.com>
2025-02-14 15:45:26 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require("electron/main");
+const path = require("path");
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+  mainWindow.loadURL('http://localhost:3000');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
